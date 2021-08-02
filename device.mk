9c42f293f20e1b592818a39aff13658cebc10307
Edgars Cīrulis <edgarsscirulis@gmail.com>
2021-08-02 03:52:03 +0300

    ginkgo: Nuke MIUI Camera & XiaomiParts,
    We are not MIUI
    We are Carbon!
    
    Signed-off-by: Edgars Cīrulis <edgarsscirulis@gmail.com>
    Change-Id: Iad964faad3b549650c42c48b6260617e7ba23456

--- device.mk
+++ device.mk
@@ -13,7 +13,6 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
 PRODUCT_PACKAGES += \
-    GinkgoMiuiCamera \
     NoCutoutOverlay \
     NotchBarKiller
 
@@ -85,9 +84,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml
-
-# Camera
-$(call inherit-product, vendor/miuicamera/config.mk)
 
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
@@ -297,9 +293,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_willow/android.hardware.nfc.hcef.xml \
     frameworks/native/data/etc/android.hardware.nfc.uicc.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_willow/android.hardware.nfc.uicc.xml
 
-# Parts
-PRODUCT_PACKAGES += \
-    XiaomiParts
+
 
 # Power
 PRODUCT_PACKAGES += \
